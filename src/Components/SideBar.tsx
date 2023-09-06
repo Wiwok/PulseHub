fe8c733c91300680fecddd6149a3db6d0d16c136
unel <enee.prog@gmail.com>
2023-09-06 23:03:48 +0200

    add side menu

--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,7 +1,18 @@
+import home from '../Assets/home.png';
+import search from '../Assets/search.png';
 function SideBar() {
 	return (
-		<div>
-
+		<div className="sideBar">
+			<div className="sideMenu">
+				<div className="sideMenuButton">
+					<img src={home} alt="home" className="sideMenuIcon" />
+					<div className="sideMenuText">home</div>
+				</div>
+				<div className="sideMenuButton">
+					<img src={search} alt="home" className="sideMenuIcon" />
+					<div className="sideMenuText">search</div>
+				</div>
+			</div>
 		</div>
 	);
 }
